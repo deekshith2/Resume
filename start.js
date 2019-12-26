a44fb18498a7748de53694be350d2fbb089b1892
Deekshith <deekshithchinna2@gmail.com>
2019-12-26 09:50:00 +0530

    new resume

---
+++ start.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+app.use(express.static('Resume'))
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/Resume/index.html'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
