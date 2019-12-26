4403661a337f0cc3c784b3fc6a43cf7b6054a5b8
Deekshith <deekshithchinna2@gmail.com>
2019-12-26 10:59:59 +0530

    new update

--- start.js
+++ start.js
@@ -3,6 +3,6 @@ const app = express()
 const port = process.env.PORT || 3000
 app.use(express.static('Resume'))
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/index.html '))
+app.get('/', (req, res) => res.sendFile(__dirname + 'index.html '))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
